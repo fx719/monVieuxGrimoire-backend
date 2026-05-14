aefb9131ee8a84a6ccbd753367ee60b4490aedfa
fx719 <frederic.dupont78@yahoo.fr>
2026-05-14 15:32:52 +0200

    feat(controllers): enhance signup security in the auth controller and User model.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,23 @@
 import { User } from "../models/User.js"
+import * as bcrypt from "bcrypt"
 
 
 export const signUp = async (req, res, next) => {
     try {
+        const passwordRegEx = /(?=.*\d)(?=.*[a-zA-Z])(?=.*[!#\$%&\?])/g
+        if (passwordRegEx.test(req.body.password) && req.body.password.length >= 8) {
+            const hashedPassword = await bcrypt.hash(req.body.password, 10)
 
-        const user = new User({
-            ...req.body
-        })
+            const user = new User({
+                email: req.body.email,
+                password: hashedPassword
+            })
 
-        await user.save()
-        res.status(201).json({ message: "Utilisateur crée avec succès" })
+            await user.save()
+            res.status(201).json({ message: "Utilisateur crée avec succès" })
+        } else {
+            throw new Error('Le mot de passe doit comporter au moins 8 caractères, dont au moins 1 lettre minuscule, 1 majuscule, 1 chiffre et un caractère spécial')
+        }
 
     } catch (error) {
         console.error(error)
--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ import uniqueValidator from 'mongoose-unique-validator'
 
 
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Merci de rentrer les données au format d'une adresse email"]
+    },
     password: { type: String, required: true }
 })
 
