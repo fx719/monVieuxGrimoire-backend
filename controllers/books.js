6cefd789374c35cbae13faa1545f722f9c2fa3d1
fx719 <frederic.dupont78@yahoo.fr>
2026-05-17 18:56:22 +0200

    fix(controllers): change multer storage to memory storage. Allow access to file buffer for a better mime-type file filtering.

--- controllers/books.js
+++ controllers/books.js
@@ -1,5 +1,7 @@
 import { Book } from '../models/Book.js'
 import * as fs from 'node:fs/promises'
+import { fileTypeFromBuffer } from 'file-type'
+import * as crypto from 'node:crypto'
 
 export const getBooks = (req, res, next) => {
     try {
@@ -14,6 +16,13 @@ export const createBook = async (req, res) => {
     try {
 
         const bookCover = req.file
+        const newFileName = Date.now() + bookCover.originalname.trim()
+        const type = await fileTypeFromBuffer(bookCover.buffer)
+
+        if (type.mime !== "image/jpg" && type.mime !== "image/jpeg" && type.mime !== "image/png") {
+            throw new Error('Erreur dans le format de fichier')
+        }
+
         const loggedInUserId = req.auth.userId
         const bookInfo = JSON.parse(req.body.book)
         delete bookInfo.userId
@@ -22,14 +31,16 @@ export const createBook = async (req, res) => {
         bookInfo.ratings[0].userId = loggedInUserId
         const uploadedBook = new Book({
             ...bookInfo,
-            imageUrl: `${req.protocol}://${req.get('host')}/images/${bookCover.filename}`
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${newFileName}`
         })
 
         uploadedBook.save()
-            .then(() => res.status(201).json({ message: "Livre ajouté avec succès" }))
-            .catch(async (error) => {
-                await fs.unlink(`./images/${bookCover.filename}`)
-                res.status(400).json({ error })
+            .then(async (book) => {
+                await fs.writeFile(`images/${newFileName}`, bookCover.buffer)
+                res.status(201).json({ message: "Livre ajouté avec succès" })
+            })
+            .catch((error) => {
+                res.status(400).json({ error: 'Les données ne sont pas au bon format' })
             })
 
     } catch (error) {
--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,28 +1,10 @@
 import multer from 'multer'
 
-const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png ': 'png'
-}
-
-
-const storage = multer.diskStorage({
+const storage = multer.memoryStorage({
     destination: (req, file, callback) => {
-
         callback(null, 'images')
-
-    },
-    filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_')
-        const extension = MIME_TYPES[file.mimetype]
-
-
-        callback(null, name + Date.now() + '.' + extension)
-
     }
 })
-
 
 export const uploadMiddleware = multer({
     storage: storage, fileFilter: (req, file, callback) => {
