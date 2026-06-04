c6fcf8ff0782bdb649b6e288187ad70989e732f2
fx719 <frederic.dupont78@yahoo.fr>
2026-06-04 16:23:26 +0200

    fix(services): delete useless try/catch in prepareBookUpload service.

--- services/prepareBookUpload.js
+++ services/prepareBookUpload.js
@@ -4,42 +4,39 @@ const fileRenamer = await import('node:crypto')
 
 export const prepareBookUpload = async (fileBuffer, req, res) => {
 
-    try {
 
-        const newFileName = fileRenamer.randomUUID()
-        const type = await fileTypeFromBuffer(fileBuffer)
+    const newFileName = fileRenamer.randomUUID()
+    const type = await fileTypeFromBuffer(fileBuffer)
 
-        if (type.mime !== "image/jpg" && type.mime !== "image/jpeg" && type.mime !== "image/png") {
-            res.status(400).json({ message: 'Mauvais format de fichier' })
-        }
-
-        const bookInfo = JSON.parse(req.body.book)
-
-        delete bookInfo.userId
-        bookInfo.userId = req.auth.userId
-
-        if (bookInfo.ratings) {
-            delete bookInfo.ratings[0].userId
-            bookInfo.ratings[0].userId = req.auth.userId
-        }
-
-        const bookInfoEntries = Object.entries(bookInfo)
-        for (const [key, value] of bookInfoEntries) {
-            if (typeof value === 'object' && key !== "ratings") {
-                throw new Error("mauvais type de données")
-            }
-        }
-
-        const bookObject = {
-            ...bookInfo,
-            imageUrl: `${req.protocol}://${req.get('host')}/images/${newFileName}.webp`
-        }
-
-        return { bookObject: bookObject, fileName: newFileName }
-
-    } catch (error) {
-        res.status(400).json({ error: "erreur lors de la préparation du fichier" })
+    if (type.mime !== "image/jpg" && type.mime !== "image/jpeg" && type.mime !== "image/png") {
+        throw new Error('Mauvais format de fichier')
     }
+
+    const bookInfo = JSON.parse(req.body.book)
+
+    delete bookInfo.userId
+    bookInfo.userId = req.auth.userId
+
+    if (bookInfo.ratings) {
+        delete bookInfo.ratings[0].userId
+        bookInfo.ratings[0].userId = req.auth.userId
+    }
+
+    const bookInfoEntries = Object.entries(bookInfo)
+    for (const [key, value] of bookInfoEntries) {
+        if (typeof value === 'object' && key !== "ratings") {
+            throw new Error("mauvais type de données")
+        }
+    }
+
+    const bookObject = {
+        ...bookInfo,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${newFileName}.webp`
+    }
+
+    return { bookObject: bookObject, fileName: newFileName }
+
+
 
 
 }
